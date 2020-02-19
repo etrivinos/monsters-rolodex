e51396c065a23e4769c25e908f61266a5f94b5a0
Edwin Alberto Triviños Ramírez <edwin.trivinos@hotmail.com>
2020-02-19 11:57:11 -0500

    Create search components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './App.css';
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search/search-box.component';
 
 class App extends React.Component {
+  monsters = [];
+
   constructor() {
     super();
     
@@ -16,13 +19,28 @@ class App extends React.Component {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(async (response) => {
         let users = await response.json();
+
+        this.monsters = users;
         this.setState({ monsters: users });
       });
+  }
+
+  onTextChanged = (searchText) => {
+    let filteredMonsters = this.monsters.filter( 
+      monster => monster.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    
+    this.setState({
+      monsters: filteredMonsters
+    });
   }
 
   render() {
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
+
+        <SearchBox placeholder={"Search monsters"} onTextChanged={this.onTextChanged} />
         <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
